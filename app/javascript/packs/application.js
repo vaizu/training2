35404ec50ceb561708118e22a84e70c3e5ee2c15
vaizu <vaizu07204869@yahoo.co.jp>
2022-04-23 11:41:18 +0000

    [header]

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,12 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'jquery'
+import "script.js"
+
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
