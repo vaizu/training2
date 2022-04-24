411513211a0a4ab7eab428695c869099162d7875
vaizu <vaizu07204869@yahoo.co.jp>
2022-04-24 08:44:17 +0000

    [info]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: "homes#top"
+
+  resources :infos, only: [:new,:show,:create,:edit,:update,:index,:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
